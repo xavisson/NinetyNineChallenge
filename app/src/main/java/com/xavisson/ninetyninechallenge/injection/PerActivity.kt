b295cea6e1e2a79d73433d5a2a9d96e767184aae
Javier del Palacio <javipalacio@gmail.com>
2018-10-30 21:29:06 +0100

    [di] Create base dependency injector boilerplate

---
+++ app/src/main/java/com/xavisson/ninetyninechallenge/injection/PerActivity.kt
@@ -0,0 +1,7 @@
+package com.xavisson.ninetyninechallenge.injection
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PerActivity
---
+++ app/src/main/java/com/xavisson/ninetyninechallenge/injection/modules/ActivityModule.kt
@@ -0,0 +1,3 @@
+package com.xavisson.ninetyninechallenge.injection.modules
+
+interface ActivityModule
