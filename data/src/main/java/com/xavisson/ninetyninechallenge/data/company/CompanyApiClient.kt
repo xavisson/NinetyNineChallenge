dfe2768669104118817a0719035a2c7d7705f3b1
Javier del Palacio <javipalacio@gmail.com>
2018-11-01 13:50:37 +0100

    [core-data] Implement company API

---
+++ data/src/main/java/com/xavisson/ninetyninechallenge/data/company/CompanyApiClient.kt
@@ -0,0 +1,22 @@
+package com.xavisson.ninetyninechallenge.data.company
+
+import com.xavisson.ninetyninechallenge.data.client.ApiClientBuilder
+import com.xavisson.ninetyninechallenge.data.client.NinetyNineApiClient
+import com.xavisson.ninetyninechallenge.domain.company.Company
+import com.xavisson.ninetyninechallenge.domain.company.CompanyApi
+import io.reactivex.Observable
+import retrofit2.http.GET
+
+class CompanyApiClient : CompanyApi {
+    override fun searchCompanies(): Observable<List<Company>> {
+        val companyApiClient = ApiClientBuilder(NinetyNineApiClient(emptySet()))
+                .buildEndpoint(CompanyApiDefinition::class)
+
+        return companyApiClient.getCompanies().map { it.toDomain() }
+    }
+}
+
+interface CompanyApiDefinition {
+    @GET("/testapi/1/companies")
+    fun getCompanies(): Observable<SearchCompaniesApiModel>
+}
